7f90c31b694d589178f4b73c56dfd0ccce33eb76
Alin Stefan Olaru <alinstefanolaru@gmail.com>
2018-12-06 21:11:53 +0100

    Added TTS

--- bot.js
+++ bot.js
@@ -20,28 +20,37 @@ chatBotApp.controller('ChatBotController', function ChatBotController (
   $timeout
 ) {
   $scope.currentMessage = ''
-
-  $scope.messages = [
-    new Message(
-      "Hello! I am a random chat bot. \n Write 'help' in chat to see what I can do",
-      false,
-      false
-    )
-  ]
+  $scope.tts = true
+  $scope.messages = []
 
   $scope.getChatClass = (isUser, isWarning) => {
     return isUser ? 'user' : isWarning ? 'warning' : 'bot'
   }
 
+  $scope.addMessage = (message, isUser) => {
+    console.log(message)
+    $scope.messages.push(new Message(message, isUser, false))
+    if ($scope.tts) {
+      window.speechSynthesis.speak(new SpeechSynthesisUtterance(message))
+    }
+  }
+
+  $scope.initMessage = () => {
+    $scope.addMessage(
+      "Hello! I am a random chat bot... Write 'help' in chat to see what I can do.",
+      false
+    )
+  }
+
   $scope.commands = {
-    hello: 'Hello there :)',
+    'hello hey greetings': 'Hello there :)',
     help:
       'This part is still under construction. You can say hello or you can ask me to tell you something about myself. You can also ask me to do something fancy, which will call an alert function.',
     about:
       'I am a random chatbuilt built by Alin Stefan Olaru for a hackaton. He liked me so much that he decided to make a standalone version of me and publish it on GitHub',
     'Who are you':
       'Hello, I am some random bot and I was made during a hackaton. Beep boop.',
-    'do something fancy': () => {
+    'something fancy': () => {
       $timeout(() => {
         $scope.messages.push(
           new Message("Not sure what I can do, but here's an alert", false)
@@ -50,14 +59,15 @@ chatBotApp.controller('ChatBotController', function ChatBotController (
       }, 500)
     }
   }
+
   $scope.chatScrollBottom = () => {
     $('#chat .body').animate({ scrollTop: $(document).height() }, 1000)
   }
+
   $scope.parseMessage = message => {
     $scope.messages.push(new Message(message, true))
     $scope.chatScrollBottom()
     $scope.currentMessage = ''
-    // if(this.commands.hasOwnProperty(message)) {
     var wordsmatch = {}
     for (var x in $scope.commands) {
       var commandWords = x.split(' ')
@@ -82,13 +92,12 @@ chatBotApp.controller('ChatBotController', function ChatBotController (
     var reply = "Sorry, I'm not sure what you mean..."
     if (highestKey !== '') reply = $scope.commands[highestKey]
     if (typeof reply === 'string') {
-      $timeout(() => $scope.messages.push(new Message(reply, false)), 500)
+      $timeout(() => {
+        $scope.addMessage(reply, false)
+      }, 500)
     } else if (typeof reply === 'function') {
       reply()
     }
     $scope.chatScrollBottom()
-    // } else {
-    // this.messages.push(new Message("Sorry, I'm not sure what you mean...", false));
-    // }
   }
 })
